0dd75bc296a6b7c2604f333d608e362020b60dc7
IgberZBN <igber_benitez@outlook.com>
2023-10-06 13:06:53 -0300

    Remove utils

--- utils/env.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    MONGO_HOST: string;
-    MONGO_DATABASE: string;
-    MONGO_DOCUMENT: string;
-  }
-}
--- utils/getEnvVar.ts
+++
@@ -1,7 +0,0 @@
-export default function getEnv(name: string): string {
-  const value = process.env[name];
-  if (typeof value === "undefined") {
-    throw new Error(`Environment variable '${name}' is not defined.`);
-  }
-  return value;
-}
