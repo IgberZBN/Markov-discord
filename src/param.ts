d7a698f9b604668cc4b205d596df47b3e817749c
IgberZBN <igber_benitez@outlook.com>
2023-10-17 19:10:23 -0300

    Create param

---
+++ src/param.ts
@@ -0,0 +1,13 @@
+interface ConfigParam {
+  wordsCount: number;
+  sampleSize: number;
+  delay: number;
+}
+
+const configParam: ConfigParam = {
+  wordsCount: 200,
+  sampleSize: 3,
+  delay: 10 * 60000,
+};
+
+export default configParam;
